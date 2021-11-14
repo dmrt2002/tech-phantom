074b8e78b1337414c6d87e4a5b1891bef9e3e00b
Tushar <72452238+dmrt2002@users.noreply.github.com>
2021-11-14 17:57:04 +0530

    Add files via upload

---
+++ precache-manifest.b211da9e23b5eeeb1d9bd7d7856bfb86.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b485005e76bbe5ea0926",
+    "url": "/tech-phantom/css/app.4b4827cf.css"
+  },
+  {
+    "revision": "9e68c8fc544e902fce50",
+    "url": "/tech-phantom/css/chunk-4aca98b3.2e560d30.css"
+  },
+  {
+    "revision": "fb2b936e0fe369bbcf63",
+    "url": "/tech-phantom/css/chunk-vendors.f8a9434f.css"
+  },
+  {
+    "revision": "9c747c0803e96152e7347c2ca74a9e36",
+    "url": "/tech-phantom/index.html"
+  },
+  {
+    "revision": "b485005e76bbe5ea0926",
+    "url": "/tech-phantom/js/app.f24e894d.js"
+  },
+  {
+    "revision": "9e68c8fc544e902fce50",
+    "url": "/tech-phantom/js/chunk-4aca98b3.a39dd636.js"
+  },
+  {
+    "revision": "fb2b936e0fe369bbcf63",
+    "url": "/tech-phantom/js/chunk-vendors.d63dabb0.js"
+  },
+  {
+    "revision": "f66732417b118924cacdedcfe1fd1bc8",
+    "url": "/tech-phantom/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/tech-phantom/robots.txt"
+  }
+]);
